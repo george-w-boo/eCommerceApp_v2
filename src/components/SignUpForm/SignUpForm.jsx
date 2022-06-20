e72cdf6f477427acfc843b6220040309162be966
Mykhailo Ostapysko <mykhailo.ostapysko@gmail.com>
2022-06-20 12:27:39 +0300

    adds generic button

--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase';
 import FormInput from '../common/FormInput/FormInput';
+import Button from '../common/Button/Button';
+
+import './SignUpForm.scss';
 
 const defaultFormFields = {
   displayName: '',
@@ -45,14 +48,15 @@ const SignUpForm = () => {
   }
 
   return (
-    <div>
-      <h1>Sign up with your email and password:</h1>
+    <div className='sign-up-container'>
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password:</span>
       <form onSubmit={handleSubmit}>
         <FormInput label='Display Name' type="text" required onChange={handleChange} name='displayName' value={displayName} />
         <FormInput label='Email' type="email" required onChange={handleChange} name='email' value={email} />
         <FormInput label='Password' type="password" required onChange={handleChange} name='password' value={password} />
         <FormInput label='Confirm Password' type="password" required onChange={handleChange} name='confirmPassword' value={confirmPassword} />
-        <button type="submit">Sign Up</button>
+        <Button type='submit'>Sign Up</Button>
       </form>
     </div>
   );
